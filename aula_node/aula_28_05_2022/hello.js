890a9cf56d843121db9aedde017722d7f14ebc66
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2022-06-04 17:01:50 -0400

    Add files via upload

---
+++ aula_node/aula_28_05_2022/hello.js
@@ -0,0 +1,20 @@
+var msg = "Hello World!";
+v_int = 50;
+v_boolean = true;
+v_float = 150.2345;
+
+console.log(msg);
+console.log(v_int);
+console.log(v_boolean);
+console.log(v_float);
+
+
+msg = 50;
+v_int = 'Teste';
+v_boolean = 150.2345;
+v_float = true;
+
+console.log(msg);
+console.log(v_int);
+console.log(v_boolean);
+console.log(v_float);
---
+++ aula_node/aula_28_05_2022/teste.js
@@ -0,0 +1,9 @@
+var a = 12;
+var b = 5;
+
+if(a == b) {
+    console.log("12 é igual a 5!!");
+
+} else {
+    console.log("a é diferente de b");
+}
